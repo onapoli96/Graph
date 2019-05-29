9e86f8a1b18a60d462aa452fbe4b94d57b2c2064
onapoli96 <o.napoli1@studenti.unisa.it>
2019-05-29 14:39:51 +0200

    english adjusted

--- src/giorgio/graph/VertexAnalyserImplements.java
+++ src/giorgio/graph/VertexAnalyserImplements.java
@@ -1,15 +1,15 @@
-package src.giorgio.graph;
+package giorgio.graph;
 
 /**
- * This class implements the interface VertexAnalyser, that contain a method to analize the vertex passed as parameter 
- * in this implementation the method print on consol the vertex
+ * This class implements the interface VertexAnalyser, that contain a method to analyze the vertex gived as parameter 
+ * in this implementation the method print on console the vertex
  * @author Giorgio
- * @param <V> generic type to define the graphos vertex
+ * @param <V> generic type to define the graphs vertex
  */
 public class VertexAnalyserImplements<V> implements VertexAnalyser<V>{
 
 	/**
-	 * The method print on consol the vertex
+	 * The method print in the console the vertex
 	 *  @param <V> generic type to define the graphos vertex
 	 */
 	@Override
